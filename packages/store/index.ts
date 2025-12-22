171cad3f368b44d3060897e53a5ae930e895f60c
Manish Daksh <manishk78625@gmail.com>
2025-12-22 20:20:18 +0530

    prisma v7 is not working

--- packages/store/index.ts
+++ packages/store/index.ts
@@ -2,10 +2,10 @@ import "dotenv/config";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "./generated/prisma/client";
 
-console.log("db" + process.env.DATABASE_URL);
+const connectionString = process.env.DATABASE_URL;
 
-const adapter = new PrismaPg({
-    connectionString : process.env.DATABASE_URL
-});
+console.log("connction string" + connectionString);
 
-export const prisma = new PrismaClient({adapter});
+const adapter = new PrismaPg({ connectionString });
+
+export const prisma = new PrismaClient({ adapter });
