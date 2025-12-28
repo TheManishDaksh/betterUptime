e9833894cd3a4a86e326c8aa7c6449e5d40d95ac
Manish Daksh <manishk78625@gmail.com>
2025-12-28 14:20:23 +0530

    drizzle orm added

--- packages/store/src/db/schema.ts
+++ packages/store/src/db/schema.ts
@@ -1,16 +1,19 @@
-import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+import { integer, pgEnum, pgTable, timestamp, varchar, type AnyPgColumn } from "drizzle-orm/pg-core";
 
 
-export const userTable = pgTable("users",{
+export const statusEnum = pgEnum("status",["up", "down", "unknown"]);
+
+export const user = pgTable("user",{
     id : integer().primaryKey().generatedAlwaysAsIdentity(),
     name : varchar({length:30}).notNull(),
     email : varchar({length:30}).unique().notNull(),
-    password : varchar({length:30}).unique().notNull(),
-    
+    password : varchar({length:30}).unique().notNull()
 })
 
-export const website = pgTable("websites",{
+export const website = pgTable("website",{
     id : integer().primaryKey().generatedAlwaysAsIdentity(),
     url : varchar({length:300}).notNull(),
-    userId : integer()
+    user_id : integer().references(()=>user.id).notNull(),
+    time_added : timestamp().defaultNow().notNull(),
+    status : statusEnum().default("unknown")
 })
