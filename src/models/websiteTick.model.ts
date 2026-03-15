f1e549d2e9e5a5634f455ffeeea646c565359e97
Manish Daksh <manishk78625@gmail.com>
2026-03-15 21:58:04 +0530

    trying to fix final bugs

--- src/models/websiteTick.model.ts
+++ src/models/websiteTick.model.ts
@@ -1,7 +1,7 @@
-import mongoose, { Document, Schema } from "mongoose";
+import mongoose, { Document, Schema, Types } from "mongoose";
 
 export interface websiteTickTypes {
-    websiteId : Schema.Types.ObjectId,
+    websiteId : Types.ObjectId,
     status : string,
     respondTime : Number,
     checkedAt : Number
--- src/worker/index.ts
+++ src/worker/index.ts
@@ -1,6 +1,9 @@
 import axios from "axios";
 import { xAckBulk, xReadGroup } from "../redis-streams";
 import { WebsiteTick } from "../models/websiteTick.model";
+import {Types} from "mongoose";
+import connectDB from "../db";
+import { ApiError } from "../utils/ApiError";
 
 const consumerGroup = "Indian-group";
 async function main(){
@@ -13,24 +16,30 @@ async function main(){
     
     const websiteStatus = res.map(({message})=> fetchWebsite(message.id, message.url));
     await Promise.all(websiteStatus);
-    console.log(websiteStatus);
     await xAckBulk(consumerGroup, res.map(({id})=>id))
     }
 }
 
 async function fetchWebsite(id:string, url:string) {
+    await connectDB().then(()=>{
+        console.log("db is also connecting on pusher");
+    }).catch((error)=>{
+        throw new ApiError(500, `your server is not running on pusher and error is ${error}`);
+    })
     const startTime = Date.now();
 
     axios.get(url)
     .then(async()=>{
         const endTime = Date.now();
         await WebsiteTick.create({
+            websiteId : new Types.ObjectId(id),
             status : "up",
             respondTime : endTime-startTime
         })
     }).catch(async()=>{
         const endTime = Date.now();
         await WebsiteTick.create({
+            websiteId : new Types.ObjectId(id),
             status : "down",
             respondTime : endTime-startTime
         })
